898f837f5bb744cc30398fed098c45da84f64ebb
likecu <noreply@qq.com>
2024-03-25 22:46:43 +0800

    #commint 修改睡眠提示

--- util/sleepy_biden.py
+++ util/sleepy_biden.py
@@ -7,9 +7,11 @@ import save_sql
 
 
 def sleepy_biden(i):
-    begin = config_helper.get_config().get("sleep_range_begin")
-    end = config_helper.get_config().get("sleep_range_end")
-    time.sleep(random.randint(begin, end))
+    begin = int(save_sql.query("sleep_range_begin"))
+    end = int(save_sql.query("sleep_range_end"))
+    sleep_time = random.randint(begin, end)
+    print("进行休眠：" + str(sleep_time) + "s")
+    time.sleep(sleep_time)
 
 
 def sleepy_biden_long():
